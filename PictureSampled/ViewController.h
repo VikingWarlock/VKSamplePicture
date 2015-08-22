b1ddcc75f1abb14d739cd64b78bfafd6741084ce
VKWK's New MacBook <vikingwarlock@gmail.com>
2015-08-22 12:29:48 +0800

    Initial Commit

---
+++ PictureSampled/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PictureSampled
+//
+//  Created by VKWK on 8/22/15.
+//  Copyright (c) 2015 VKWK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PictureSampled/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PictureSampled
+//
+//  Created by VKWK on 8/22/15.
+//  Copyright (c) 2015 VKWK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
